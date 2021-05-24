d2b6daafdd7e3710b439f70a522911ef4fc16f31
Björn Ekryd <bjornatekryd.se>
2021-05-24 08:41:06 +0200

    Added integration test for normal sorting and skip sorting

---
+++ maven-plugin/src/it/normal-sort/postbuild.groovy
@@ -0,0 +1,13 @@
+log = new File(basedir, 'build.log')
+sorted = new File(basedir, 'basic_products.graphqls')
+expected = new File(basedir, 'expected.graphqls')
+backup = new File(basedir, 'basic_products.graphqls.bak')
+
+assert log.exists()
+assert log.text.contains('Sorting file ' + sorted.absolutePath)
+assert log.text.contains('Saved backup of ' + sorted.absolutePath + ' to ' + backup.absolutePath)
+assert log.text.contains('Saved sorted schema file to ' + sorted.absolutePath)
+assert backup.exists()
+assert expected.text.tokenize('\n').equals(sorted.text.replaceAll('\r','').tokenize('\n'))
+
+return true
